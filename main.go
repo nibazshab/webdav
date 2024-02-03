24349d03ee1443be8bde0f297639f619e2f1d66f
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-02-03 21:52:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,63 +2,53 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
     "os"
     "path/filepath"
-    "strconv"
 
-    "golang.org/x/net/context"
     "golang.org/x/net/webdav"
 )
 
-func handleDirList(fs webdav.FileSystem, w http.ResponseWriter, req *http.Request) bool {
-    ctx := context.Background()
-    f, _ := fs.OpenFile(ctx, req.URL.Path, os.O_RDONLY, 0)
-    defer f.Close()
-    if fi, _ := f.Stat(); fi != nil && !fi.IsDir() {
-        return false
-    }
-    dirs, _ := f.Readdir(-1)
-
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    fmt.Fprintf(w, "<pre>\n")
-    for _, d := range dirs {
-        name := d.Name()
-        if d.IsDir() {
-            name += "/"
-        }
-        size := ""
-        if !d.IsDir() {
-            size = strconv.FormatInt(d.Size(), 10)
-        }
-        fmt.Fprintf(w, "<a href=\"%s\">%s</a> %s\n", name, name, size)
-    }
-    fmt.Fprintf(w, "</pre>\n")
-    return true
-}
-
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Please provide a directory path.")
+    port := "8088"
+    var rootfs string
+
+    o := len(os.Args)
+    if o == 1 {
+        fmt.Println("WARNING: Using the default directory (./)")
+        rootfs = "."
+    } else if o == 2 {
+        rootfs = os.Args[1]
+    } else {
+        fmt.Println("ERROR: Just need 1 parameter")
         return
     }
 
-    rootDir := os.Args[1]
-    absRootDir, _ := filepath.Abs(rootDir)
+    rootfs, _ = filepath.Abs(rootfs)
 
     fs := &webdav.Handler{
-        FileSystem: webdav.Dir(absRootDir),
+        FileSystem: webdav.Dir(rootfs),
         LockSystem: webdav.NewMemLS(),
     }
 
-    http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-        if req.Method == "GET" && handleDirList(fs.FileSystem, w, req) {
+    fmt.Println("port:", port, "\nrootfs:", rootfs)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        switch r.Method {
+        case "PROPPATCH", "MKCOL", "DELETE", "PUT", "COPY", "MOVE", "LOCK", "UNLOCK":
+            http.Error(w, "Permission denied", http.StatusForbidden)
             return
         }
-        fs.ServeHTTP(w, req)
+
+        fs.ServeHTTP(w, r)
+
+        xff := r.Header.Get("X-Forwarded-For")
+        if xff == "" {
+            xff = r.RemoteAddr
+        }
+        log.Print(xff + " - " + r.Method + " - " + r.URL.String() + " - " + r.Header.Get("user-agent"))
     })
 
-    addr := "8088"
-    fmt.Printf("WebDAV server listening on %s\n", addr)
-    _ = http.ListenAndServe(":"+addr, nil)
+    http.ListenAndServe(":"+port, nil)
 }
