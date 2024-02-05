deb5c6a9a58c0eb3a78a5cd5d6996125d2f07a0a
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2024-02-05 17:37:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,54 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "path/filepath"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 
-    "golang.org/x/net/webdav"
+	"golang.org/x/net/webdav"
 )
 
 func main() {
-    port := "8088"
-    var rootfs string
+	port := "8088"
+	web := "/web/"
 
-    o := len(os.Args)
-    if o == 1 {
-        fmt.Println("WARNING: Using the default directory (./)")
-        rootfs = "."
-    } else if o == 2 {
-        rootfs = os.Args[1]
-    } else {
-        fmt.Println("ERROR: Just need 1 parameter")
-        return
-    }
+	var rootfs string
+	o := len(os.Args)
 
-    rootfs, _ = filepath.Abs(rootfs)
+	if o == 1 {
+		fmt.Println("WARNING: Using the default directory (./)")
+		rootfs = "."
+	} else if o == 2 {
+		rootfs = os.Args[1]
+	} else {
+		fmt.Println("ERROR: Just need 1 parameter")
+		return
+	}
 
-    fs := &webdav.Handler{
-        FileSystem: webdav.Dir(rootfs),
-        LockSystem: webdav.NewMemLS(),
-    }
+	rootfs, _ = filepath.Abs(rootfs)
 
-    fmt.Println("port:", port, "\nrootfs:", rootfs)
+	fs := &webdav.Handler{
+		FileSystem: webdav.Dir(rootfs),
+		LockSystem: webdav.NewMemLS(),
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        switch r.Method {
-        case "PROPPATCH", "MKCOL", "DELETE", "PUT", "COPY", "MOVE", "LOCK", "UNLOCK":
-            http.Error(w, "Permission denied", http.StatusForbidden)
-            return
-        }
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "PROPPATCH", "MKCOL", "DELETE", "PUT", "COPY", "MOVE", "LOCK", "UNLOCK":
+			http.Error(w, "Permission denied", http.StatusForbidden)
+			return
+		}
 
-        fs.ServeHTTP(w, r)
+		if len(r.URL.Path) >= len(web) && r.URL.Path[:len(web)] == web {
+			http.StripPrefix(web, http.FileServer(http.Dir(rootfs))).ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
 
-        xff := r.Header.Get("X-Forwarded-For")
-        if xff == "" {
-            xff = r.RemoteAddr
-        }
-        log.Print(xff + " - " + r.Method + " - " + r.URL.String() + " - " + r.Header.Get("user-agent"))
-    })
+		logRecord(r)
+	})
 
-    http.ListenAndServe(":"+port, nil)
+	fmt.Println("port:", port, "\nrootfs:", rootfs, "\nweb:", web)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func logRecord(r *http.Request) {
+	log.Print(r.RemoteAddr + " - " + r.Method + " - " + r.URL.String() + " - " + r.Header.Get("user-agent"))
 }
